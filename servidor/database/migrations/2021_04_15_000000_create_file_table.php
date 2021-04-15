fee6e7efffd602b0eb34532365aec8c3af56fc91
aythami <aythamiza@gmail.com>
2021-04-15 16:59:15 +0100

    preubas de migracion de laravel 15/04/2021 1h

---
+++ servidor/database/migrations/2021_04_15_000000_create_file_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('tipe');
+            $table->date('date_file');
+            $table->date('date_up');
+
+            $table->unsignedInteger('users_id');
+            $table->foreign('users_id')->references('id')->on('users')->onUpdate('RESTRICT')->onDelete('RESTRICT');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file');
+    }
+}

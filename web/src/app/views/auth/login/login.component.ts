c96d5238df228bea0681b7cc94f6b84c669fdb2f
Javier Martin <javiermartin.gara@gmail.com>
2021-05-16 18:05:20 +0100

    Control fallo en el Login y Control para evitar usuarios Repetidos 3h

--- web/src/app/views/auth/login/login.component.ts
+++ web/src/app/views/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { StorageService } from '../../../core/service/storage/storage.service';
 import { AuthService } from '../../../core/service/auth/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-auth-login',
@@ -12,14 +13,20 @@ import { AuthService } from '../../../core/service/auth/auth.service';
 export class LoginComponent implements OnInit {
 
   public hidePassword: boolean;
+  private durationInSeconds: number = 5;
   private formLogin: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService, private storage: StorageService, private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private storage: StorageService, private _snackBar: MatSnackBar, private router: Router) { }
 
   ngOnInit() {
     this.hidePassword = true;
     this.formLogin = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [
+        Validators.required, 
+        Validators.email,
+        Validators.pattern('[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}')
+        ]
+      ],
       password: ['', Validators.required]
     });
     this.storage.ver();
@@ -38,9 +45,19 @@ export class LoginComponent implements OnInit {
           this.router.navigate(['/home']);
         },
         err => {
-          console.log(err);
+          console.log(err.error);
+          this.errorLoginUser(err.error)
         }
       );
+    }
+  }
+
+  errorLoginUser(errors: any) {
+    if (errors.message){
+      let message = 'El e-mail o la contraseña no son correctos.';
+      this._snackBar.open(message, '', {
+        duration: this.durationInSeconds * 1000,
+      });
     }
   }
 
@@ -48,7 +65,7 @@ export class LoginComponent implements OnInit {
     if (this.formLogin.get('email').hasError('required')) {
       return 'Este campo no puede estar vacío';
     }
-    if (this.formLogin.get('email').hasError('email')) {
+    if (this.formLogin.get('email').hasError('email') || this.formLogin.get('email').hasError('pattern')) {
       return 'Escribe un E-mail correcto';
     }
     return '';
